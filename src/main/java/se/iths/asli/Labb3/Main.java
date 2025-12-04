703acf1449a83604e604e7dc941a73336cd2c34d
AMT94-star <aslihan.taskin94@gmail.com>
2025-12-04 13:09:43 +0100

    Skapat instans av DAO samt anropat metoderna i Main

--- src/main/java/se/iths/asli/Labb3/Main.java
+++ src/main/java/se/iths/asli/Labb3/Main.java
@@ -2,5 +2,19 @@ package se.iths.asli.Labb3;
 
 public class Main {
     public static void main(String[] args) {
+        PersonDAO dao = new PersonDAOImpl();
+
+        Person newPerson = new Person("Boromir", "Gondor", "1950-05-05", 30000.0);
+
+        System.out.println("Hämtar alla personer");
+        for (Person p : dao.findAll()) {
+            System.out.println(
+                    p.getPersonId() + " " +
+                            p.getFirstName() + " " +
+                            p.getLastName() + " " +
+                            p.getDob() + " " +
+                            p.getIncome()
+            );
+        }
     }
 }
